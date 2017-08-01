a4f765f148d231f041e9a774b55d429222eb3d7f
Patrick Kimber <code@pkimber.net>
2017-08-01 19:15:26 +0100

    model-hook

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -23,10 +23,14 @@ test('should link to contact information.', function (assert) {
   click('a:contains("Contact")');
   andThen(function() {
     assert.equal(currentURL(), '/contact', 'should navigate to contact');
-  });  
+  });
 });
 
 test('should list available rentals.', function (assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });  
 });
 
 test('should filter the list of rentals by city.', function (assert) {
