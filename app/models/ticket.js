bba532746db1a66747ae8ef28c6d5294ba9673cb
Patrick Kimber <code@pkimber.net>
2017-08-30 06:07:41 +0100

    Ticket has a contact and use Semantic cards

--- app/models/ticket.js
+++ app/models/ticket.js
@@ -1,6 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  contact: DS.belongsTo('contact'),
   title: DS.attr('string'),
   priority: DS.attr('string'),
   username: DS.attr('string')
