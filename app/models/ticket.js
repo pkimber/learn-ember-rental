ae283c73210cdeeb7b11b1f20979227aa6b21a8f
Patrick Kimber <code@pkimber.net>
2017-08-30 07:14:02 +0100

    Set ticket priority

--- app/models/ticket.js
+++ app/models/ticket.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   contact: DS.belongsTo('contact'),
   title: DS.attr('string'),
-  priority: DS.attr('string'),
+  priority: DS.belongsTo('priority'),
   username: DS.attr('string')
 });
